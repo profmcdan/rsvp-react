a52562c5e097215fc0819aad8be894b611a857c9
Daniel Ale <danielale9291@gmail.com>
2018-12-06 17:09:11 +0100

    feature/added state management/0001

--- src/components/GuestList.js
+++ src/components/GuestList.js
@@ -1,39 +1,44 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Guest from "./Guest";
 
 class GuestList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
 	render() {
+		const { guests, toggleConfirmationAt, toggleEditingAt, setNameAt } = this.props;
 		return (
 			<ul>
 				<li className="pending">
 					<span>Safia</span>
 				</li>
-				<li className="responded">
-					<span>Iver</span>
-					<label>
-						<input type="checkbox" checked /> Confirmed
-					</label>
-					<button>edit</button>
-					<button>remove</button>
-				</li>
-				<li className="responded">
-					<span>Corrina</span>
-					<label>
-						<input type="checkbox" checked /> Confirmed
-					</label>
-					<button>edit</button>
-					<button>remove</button>
-				</li>
-				<li>
-					<span>Joel</span>
-					<label>
-						<input type="checkbox" /> Confirmed
-					</label>
-					<button>edit</button>
-					<button>remove</button>
-				</li>
+
+				{guests.map((guest, index) => {
+					return (
+						<Guest
+							key={index}
+							name={guest.name}
+							isConfirmed={guest.isConfirmed}
+							isEditing={guest.isEditing}
+							handleConfirmation={() => toggleConfirmationAt(index)}
+							handleEditing={() => toggleEditingAt(index)}
+							setName={(text) => setNameAt(index)}
+						/>
+					);
+				})}
 			</ul>
 		);
 	}
 }
+
+GuestList.propTypes = {
+	guests: PropTypes.array.isRequired,
+	toggleConfirmationAt: PropTypes.func.isRequired,
+	toggleEditingAt: PropTypes.func.isRequired,
+	setNameAt: PropTypes.func.isRequired
+};
 
 export default GuestList;
