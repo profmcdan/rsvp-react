09727f0c0dcc5689d85d984373b13bca31a8391a
Daniel Ale <danielale9291@gmail.com>
2018-12-06 13:09:21 +0100

    Added a basic stateless UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,96 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		guests: [
+			{
+				name: "Treasure",
+				isConfirmed: false,
+				id: 1
+			},
+			{
+				name: "Nic",
+				isConfirmed: true,
+				id: 2
+			}
+		]
+	};
+
+	getTotalInvited = () => {
+		return this.state.guests.length;
+	};
+
+	// getAttendingGuests = () => {}
+	// getConfirmedGuests = () => {}
+	render() {
+		return (
+			<div className="App">
+				<header>
+					<h1>RSVP</h1>
+					<p>A McDan App</p>
+					<form>
+						<input type="text" value="Safia" placeholder="Invite Someone" />
+						<button type="submit" name="submit" value="submit">
+							Submit
+						</button>
+					</form>
+				</header>
+				<div className="main">
+					<div>
+						<h2>Invitees</h2>
+						<label>
+							<input type="checkbox" /> Hide those who havent responded
+						</label>
+					</div>
+					<table className="counter">
+						<tbody>
+							<tr>
+								<td>Attending:</td>
+								<td>2</td>
+							</tr>
+							<tr>
+								<td>Unconfirmed:</td>
+								<td>1</td>
+							</tr>
+							<tr>
+								<td>Total:</td>
+								<td>3</td>
+							</tr>
+						</tbody>
+					</table>
+					<ul>
+						<li className="pending">
+							<span>Safia</span>
+						</li>
+						<li className="responded">
+							<span>Iver</span>
+							<label>
+								<input type="checkbox" checked /> Confirmed
+							</label>
+							<button>edit</button>
+							<button>remove</button>
+						</li>
+						<li className="responded">
+							<span>Corrina</span>
+							<label>
+								<input type="checkbox" checked /> Confirmed
+							</label>
+							<button>edit</button>
+							<button>remove</button>
+						</li>
+						<li>
+							<span>Joel</span>
+							<label>
+								<input type="checkbox" /> Confirmed
+							</label>
+							<button>edit</button>
+							<button>remove</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
